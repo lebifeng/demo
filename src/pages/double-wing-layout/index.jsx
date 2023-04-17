fc1bea24c5e9e8c2fcce8ebce9b1c6108c6a4a9b
bifeng le <bifengle@bifengdeMac-mini.local>
2023-04-17 20:17:01 +0800

    feat: 优化

--- src/pages/double-wing-layout/index.jsx
+++ src/pages/double-wing-layout/index.jsx
@@ -5,7 +5,9 @@ import classes from './index.module.css';
 function DoubleWingLayout() {
   return (
     <div className={classes.container}>
-      <div className={cx(classes.content, classes.item)}>content</div>
+      <div className={cx(classes.center, classes.item)}>
+        <div className={classes.content}>center</div>
+      </div>
       <div className={cx(classes.left, classes.item)}>left</div>
       <div className={cx(classes.right, classes.item)}>right</div>
     </div>
